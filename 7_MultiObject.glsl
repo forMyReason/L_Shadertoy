17178a09c5b615ee2e12fd4ad7775094f2b1bc2c
LiDanyang <light_SM@163.com>
2025-05-25 22:27:33 +0800

    feat: 7.1 增加多个物体

---
+++ 7_MultiObject.glsl
@@ -0,0 +1,80 @@
+// #define和const 之间存在差异
+const int MAX_MARCHING_STEPS = 255;
+const float MIN_DIST = 0.0;
+const float MAX_DIST = 100.0;
+const float PRECISION = 0.001;
+
+// 计算射线的点到球心的距离，返回 SignDistance
+float sdSphere(vec3 p, float r, vec3 offset)
+{
+    return length(p - offset) - r;
+}
+
+// 返回场景最近的形状
+float sdScene(vec3 p)
+{
+    // TODO: 7.1 添加多个物体
+    float sphere_left = sdSphere(p, 1.0, vec3(-1.5, 0, -2));
+    float sphere_right = sdSphere(p, 1.0, vec3( 1.5 ,0, -2));
+    return min(sphere_left, sphere_right);
+}
+
+float rayMarch(vec3 ro, vec3 rd, float start, float end)
+{
+    float depth = start;
+
+    for(int i = 0; i < MAX_MARCHING_STEPS; i++)
+    {
+        vec3 p = ro + depth * rd;
+        float d = sdScene(p);
+        depth += d;
+        if(d < PRECISION || depth > end)
+        {
+            break;
+        }
+    }
+    return depth;
+}
+
+vec3 calcNormal_swizzling(vec3 p) {
+  vec2 e = vec2(1.0, -1.0) * 0.0005; // epsilon
+  float r = 1.; // radius of sphere
+  return normalize(
+    e.xyy * sdScene(p + e.xyy) +
+    e.yyx * sdScene(p + e.yyx) +
+    e.yxy * sdScene(p + e.yxy) +
+    e.xxx * sdScene(p + e.xxx));
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
+    vec3 backgroundColor = vec3(0.835, 1, 1);
+
+    vec3 col = vec3(0);
+
+    vec3 ro = vec3(0,0,3);
+    vec3 rd = normalize(vec3(uv,-1));       // 每个像素不同的光线方向
+
+    float depth = rayMarch(ro, rd, MIN_DIST, MAX_DIST);
+
+    if(depth > MAX_DIST)
+    {
+        col = backgroundColor;
+    }
+    else
+    {
+        vec3 p = ro + rd * depth;
+        vec3 normal = calcNormal_swizzling(p);
+
+        vec3 lightPosition = vec3(2,2,7);
+        vec3 lightDirection = normalize(lightPosition - p);
+
+        float dif = clamp(dot(normal, lightDirection),0.3,1.0);
+
+        col = dif * vec3(1, 0.58, 0.29) + backgroundColor * .2;
+    }
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # L_Shadertoy
 上班敲敲shadertoy，打发时间用...
+
+
+
+## 0. 工具汇总
+- https://www.desmos.com/calculator?lang=zh-CN
+- https://www.desmos.com/matrix?lang=zh-CN
+- https://www.wolframalpha.com/
+- https://www.wolframalpha.com/input/?i=%5B%5Bcos%28t%29%2C-sin%28t%29%5D%2C%5Bsin%28t%29%2Ccos%28t%29%5D%5D+*%5Bx%2Cy%5D
+- https://registry.khronos.org/OpenGL-Refpages/gl4/html/mix.xhtml
+- https://zhuanlan.zhihu.com/p/52287086
+
+
+## 使用SDF画一个圆
+- 调整圆形比例，不随着窗口改变纵横比
+- 使用iTime让圆随着时间改变颜色
+- 让圆移动
+- 空心圆【？】
+
+## 画正方形
+- 怎么让正方向不管怎么拖动，正方形都在所有屏幕中间？
+- 让正方向旋转
+
+## 多个2D形状的混合
+- mix / lerp
+- 2D SDF就是公式
+- 两次mix混合多种形状
+- 最好的sdf就是返回距离，每个draw不反悔vec3
+- 新增渐变背景
+
+## 2D SDF
+- 常见的各种2D SDF公式
+- 2D SDF 操作
+
+## 6. RayMarching
+- 坐标系
+    - shadertoy采用右手坐标系。
+    - x 轴沿画布的水平轴，y 轴沿画布的垂直轴，并假设摄像机朝向 -z
+- 光线算法之间的区别
+    - ray casting
+    - ray marching
+    - ray tracing
+    - path tracing
+- RayMarching原理
+    - https://www.youtube.com/watch?v=nMAgogUyv3E
+    - https://www.desmos.com/calculator/ragaytzefw
+
+
+## 7.1 绘制多个3D对象
+- 添加多个物体
+    - 使用 min 对 sdSphere 返回的最近距离操作
